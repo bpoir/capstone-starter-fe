720a01e0ca94863ecd8386cee54e9d5c4d0a8c75
Liz <liz.alessandri90@gmail.com>
2023-02-21 15:21:32 -0500

    add buttons to admin protected page

--- src/pages/protected/Protected.js
+++ src/pages/protected/Protected.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import mustBeAuthenticated from "../../redux/hoc/mustBeAuthenticated";
 import Header from "../../components/header/Header";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 class Protected extends Component {
 
@@ -9,11 +11,22 @@ class Protected extends Component {
             <div className="Protected">
                 <Header isAuthenticated={this.props.isAuthenticated} />
                 <div className="container">
-                    <h2>Protected Content</h2>
+                    <h2>Admin Page</h2>
+                    <div class="row">
+                    <div class="col ms-5">
+                        <button /*</div>onclick={ this.getForm }*/>Add/Manage Users</button>
+                    </div>
+                    <div class="col">
+                        <button /*</div>onclick={ this.getForm }*/>Add/Manage Products</button>
+                    </div>
                 </div>
+                </div>
+
             </div>
         );
     }
 }
+
+
 
 export default mustBeAuthenticated(Protected);
