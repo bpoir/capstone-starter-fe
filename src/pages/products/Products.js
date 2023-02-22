70c87427bda13251f93f3a5ec7618142467f15c6
chris delp <christopher.delp@libertymutual.com>
2023-02-22 11:25:45 -0500

    Added Products API Call

--- src/pages/products/Products.js
+++ src/pages/products/Products.js
@@ -11,36 +11,22 @@ class Products extends Component {
     state = {
         errorMessage: null,
         formData: {
-            firstName: "",
-            lastName: "",
-            email: "",
-            password: ""
+            productName: "",
+            productType: "",
+            productAllergens: "",
+            productPrice: ""
         }
     }
 
     //method that handles updating the data in state that matches the data in the form
     //runs everytime a form field changes
-    handleChange = (event) => {
-        //create a new object from form data in state
-        let formData = {...this.state.formData};
-
-        //take what is changed in the form and update the mathcing key in the form data object
-        formData[event.target.id] = event.target.value;
-
-        //update formData in state with the new object
-        this.setState({formData});
-    }
+    
 
     //run when the form is submitted
     handleSubmit = (event) => {
 
         //prevent the form from refreshing the page
         event.preventDefault();
-
-        if(this.state.formData.password && this.state.formData.password !== event.target.confirmPassword.value){
-            this.setState({errorMessage: "The supplied passwords must match!"})
-            return;
-        }
 
         //get API url from the environment variables
         const apiURL = process.env.REACT_APP_API_URL
@@ -50,21 +36,28 @@ class Products extends Component {
         fetch(`${apiURL}/api/products`, {
             method: "GET", //make sure whe set our method to POST when creating records
             headers: {
-                'content-type': 'application/json' //make sure we set the content-type headers so the API knows it is recieveing JSON data
-            },
-            body: JSON.stringify(this.state.formData) //send our data form state int he body of the request
+                'accept': 'application/json' //make sure we set the content-type headers so the API knows it is recieveing JSON data
+             },
+            // body: JSON.stringify(this.state.formData) //send our data form state int he body of the request
         })
         .then((response) => response.json()) // on success, turn the respons into JSON so we can work with it
         .then((data) => {
-            const message = "Registration successful! Please sign-in"
+            const message = "Pulled Product Data"
             //programatically redirect to another route on success
-            this.props.history.push(`/login?message=${message}`)
+            this.props.history.push(`/products?message=${message}`)
+            console.log(data)
+            this.setState({
+                formData: {...this.state.formData, ...data}
+            });
         })
+        
         .catch(e => console.log(e.message)) //console.log any errors if the previous steps fail
 
     }
 
     render() {
+        const params = new URLSearchParams(this.props.location.search);
+        const flashMessage = params.get('message');
         return (
             <div className="Products">
 
@@ -72,11 +65,12 @@ class Products extends Component {
 
                 <div className="container">
                     {this.state.errorMessage && <Alert variant="danger">{this.state.errorMessage}</Alert>}
+                    {flashMessage && <Alert variant="info">{flashMessage}</Alert>}
                 </div>
                 
                 <h3 className="text-center" >Get Products</h3>
                 <GetProducts
-                    handleChange={this.handleChange}
+                    
                     handleSubmit={this.handleSubmit}
                     formData={this.state.formData}
                 />
